338f7448ef6e4b7f4a3106b54eaaa200646a8201
Hafizh Alfaris <hafizh.polsub@pratama.tech>
2024-08-22 11:14:42 +0700

    route

--- routes/web.php
+++ routes/web.php
@@ -53,7 +53,6 @@ Route::middleware([AutoLogout::class])->group(function () {
             Route::post('/updatejadwal/{id}', [ScheduleController::class, 'update'])->name('updatejadwal');
         });
 
-<<<<<<< HEAD
         // Manage Leave
         Route::prefix('leave.kelolacuti')->group(function () {
             Route::get('/', [AdminController::class, 'cuti'])->name('kelolacuti');
@@ -62,15 +61,10 @@ Route::middleware([AutoLogout::class])->group(function () {
             Route::get('/editcuti', [AdminController::class, 'editcuti'])->name('editcuti');
         });
     });
-=======
-    // Manage Leave
-    Route::get('/kelolacuti', [AdminController::class, 'cuti'])->name('kelolacuti');
-});
->>>>>>> cb1d634146a4e8e408136bdf6c51d42505ab26bc
 
 
-Route::get('/import', [ImportexcelController::class,'index']);
-Route::post('/import/excel', [ImportexcelController::class,'post'])->name('post-excel');
+    Route::get('/import', [ImportexcelController::class, 'index']);
+    Route::post('/import/excel', [ImportexcelController::class, 'post'])->name('post-excel');
 
     Route::group(['prefix' => 'pegawai', 'middleware' => ['pegawai'], 'as' => 'pegawai.'], function () {
 
