eea09482722544b240dd672ff2e37b11becf3290
Hafizh Alfaris <hafizh.polsub@pratama.tech>
2024-10-24 08:35:56 +0700

    init

--- app/Http/Controllers/API/UserController.php
+++ app/Http/Controllers/API/UserController.php
@@ -6,17 +6,22 @@ use App\Http\Controllers\Controller;
 use App\Models\User;
 use Illuminate\Http\Request;
 
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Str;
+
 class UserController extends Controller
 {
     public function index(Request $request)
     {
-        $limit = $request->limit ?? 10;
+        $limit = $request->get('limit', 10);
 
-        $users = User::where('role', 2)->paginate($limit)
+        $users = User::select('id', 'name', 'role', 'token')
+            ->orderBy('name', 'asc')
+            ->paginate($limit)
             ->appends(['limit' => $limit]);
 
         return response()->json([
-            'status' => 'ok',
+            'success' => true,
             'data' => $users->items(),
             'pagination' => [
                 'current_page' => $users->currentPage(),
@@ -29,68 +34,125 @@ class UserController extends Controller
         ]);
     }
 
+    // mending langsung di-hit dari frontend enterprisely?
+    // public function index(Request $request)
+    // {
+    //     $length = $request->get('length', 10); // limit data
+    //     $start = $request->get('start', 0); // DataTables sends 'start' to indicate offset
+
+    //     $currentPage = ($start / $length) + 1;
+
+    //     $users = User::select('id', 'name', 'role', 'token')
+    //         ->orderBy('name', 'asc')
+    //         ->paginate($length, ['*'], 'page', $currentPage);
+
+    //     // $baseUrl = request()->getSchemeAndHttpHost() . $request->getBaseUrl();
+
+    //     return response()->json([
+    //         'success' => true,
+    //         'recordsTotal' => $users->total(),
+    //         'recordsFiltered' => $users->total(),
+    //         'data' => $users->items(),
+    //         'pagination' => [
+    //             'current_page' => $users->currentPage(),
+    //             'total_pages' => $users->lastPage(),
+    //             'total_items' => $users->total(),
+    //             'per_page' => $users->perPage(),
+    //             'next_page_url' => $users->nextPageUrl(),
+    //             'prev_page_url' => $users->previousPageUrl(),
+    //         ]
+    //     ]);
+    // }
+
     public function store(Request $request)
     {
-
-
-        //TODO: tambahin key yang mau disimpen, cek json
         try {
             $validatedData = $request->validate([
                 'name' => 'required|string|max:255',
                 'email' => 'required|string|email|max:255|unique:users',
                 'password' => 'required|string|min:6',
+                'username' => 'required|string|max:255|unique:users',
+                'telephone' => 'required|string|max:15',
+                'place_of_birth' => 'required|string|max:255',
+                'date_of_birth' => 'required|date',
+                'gender' => 'required|string|in:Laki - laki,Perempuan',
+                'religion' => 'required|string|max:50',
+                'address' => 'required|string|max:500',
+                'position' => 'nullable|string|max:255',
+                'id_card' => 'required|string|size:16|unique:users',
+                // 'role' => 'required|integer',
+                // 'avatar' => 'nullable|string|max:255',
+                'status' => 'required|in:0,1',
+                'available' => 'required|integer',
+                'schedule' => 'required|integer',
+                'delete' => 'required|in:0,1',
+                // 'token' => 'required|string|max:255|unique:users',
             ]);
 
             $user = User::create([
                 'name' => $validatedData['name'],
                 'email' => $validatedData['email'],
-                'password' => bcrypt($validatedData['password']),
-                'role' => 2,
+                'password' => Hash::make($validatedData['password']),
+                'username' => $validatedData['username'],
+                'telephone' => $validatedData['telephone'],
+                'place_of_birth' => $validatedData['place_of_birth'],
+                'date_of_birth' => $validatedData['date_of_birth'],
+                'gender' => $validatedData['gender'],
+                'religion' => $validatedData['religion'],
+                'address' => $validatedData['address'],
+                'position' => $validatedData['position'] ?? null,
+                'id_card' => $validatedData['id_card'],
+                'role' => $validatedData['role'],
+                'avatar' => $validatedData['avatar'] ?? null,
+                'status' => $validatedData['status'],
+                'available' => $validatedData['available'],
+                'schedule' => $validatedData['schedule'],
+                'delete' => $validatedData['delete'],
+                'token' => Str::random(10),
             ]);
 
             return response()->json([
-                'status' => 'ok',
+                'success' => true,
                 'message' => 'Successfully created',
                 'data' => $user,
             ], 201);
         } catch (\Illuminate\Validation\ValidationException $err) {
             return response()->json([
-                'status' => 'error',
+                'success' => false,
                 'message' => 'Validation error',
                 'errors' => $err->errors(),
             ], 422);
         } catch (\Exception $err) {
             return response()->json([
-                'status' => 'error',
+                'success' => false,
                 'message' => 'Failed to create user',
             ], 500);
         }
     }
 
-
     // Show a specific user
     public function show($id)
     {
-        $user = User::where('id', $id)->where('role', 2)->get();
+        $user = User::find($id, ['id', 'name', 'token']);
         if (!$user) {
             return response()->json([
-                'status' => 'error',
+                'success' => false,
                 'message' => 'User not found'
             ], 404);
         }
 
         return response()->json([
-            'status' => 'ok',
+            'success' => true,
             'data' => $user,
         ], 200);
     }
 
     public function update(Request $request, $id)
     {
-        $user = User::where('id', $id)->where('role', 2)->get();
+        $user = User::find($id);
         if (!$user) {
             return response()->json([
-                'status' => 'error',
+                'success' => false,
                 'message' => 'User not found'
             ], 404);
         }
@@ -98,25 +160,26 @@ class UserController extends Controller
         try {
             $validatedData = $request->validate([
                 'name' => 'required|string|max:255',
-                'email' => 'required|string|email|max:255|unique:users,email,' . $user->id,
-                // 'password' => 'required|string|min:6',
+                // 'role' => 'required|integer',
             ]);
 
             $user->update($validatedData);
             return response()->json([
-                'status' => 'ok',
+                'success' => true,
                 'message' => 'Successfully updated',
-                'data' => $user,
+                'data' => [
+                    'name' => $user->name,
+                ],
             ], 200);
         } catch (\Illuminate\Validation\ValidationException $err) {
             return response()->json([
-                'status' => 'error',
+                'success' => false,
                 'message' => 'Validation error',
                 'errors' => $err->errors(),
             ], 422);
         } catch (\Exception $err) {
             return response()->json([
-                'status' => 'error',
+                'success' => false,
                 'message' => 'Failed to update user',
             ], 500);
         }
@@ -125,10 +188,10 @@ class UserController extends Controller
     // Delete a user
     public function destroy($id)
     {
-        $user = User::where('id', $id)->where('role', 2)->get();
+        $user = User::find($id);
         if (!$user) {
             return response()->json([
-                'status' => 'error',
+                'success' => false,
                 'message' => 'User not found'
             ], 404);
         }
@@ -136,12 +199,12 @@ class UserController extends Controller
         try {
             $user->delete();
             return response()->json([
-                'status' => 'ok',
+                'success' => true,
                 'message' => 'Successfully deleted',
             ], 200);
         } catch (\Exception $err) {
             return response()->json([
-                'status' => 'error',
+                'success' => false,
                 'message' => 'Failed to update data',
             ], 500);
         }
